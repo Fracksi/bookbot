f6022a559cf6ca5bd0115c3e7d808bd277cb570c
fracksi <fracksi@example.com>
2025-05-14 01:44:15 -0500

    Final changes for main program with some readability improvements.

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 #!/usr/bin/env python
 
 import stats
+import sys
 
 
 def main():
-    bookFileLocation = "books/frankenstein.txt"
-    try:
-      print("============ BOOKBOT ============")
-      print(f"Analyzing book found at {bookFileLocation}...")
+  try:
+    bookFileLocation = sys.argv[1]
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {bookFileLocation}...")
 
-      # Prints the total number of words in the book
-      print("----------- Word Count ----------")
-      totalWordCount = stats.get_book_wordCount(bookFileLocation)
-      print(f"Found {totalWordCount} total words")
+    # Prints the total number of words in the book
+    print("----------- Word Count ----------")
+    totalWordCount = stats.get_book_wordCount(bookFileLocation)
+    print(f"Found {totalWordCount} total words")
 
-      # Prints the total number of characters in the book, per character count
-      print("----------- Character Count ----------")
-      totalCharCount = stats.get_book_charCounts(bookFileLocation)
-      for char in totalCharCount:
-        print(f"{char['char']}: {char['num']}")
-
-      # Prints the entirety of the book text to console
-      # printedTextOut = stats.get_book_text(bookFileLocation)
-      # print(printedTextOut)
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    # Prints the total number of characters in the book, per character count
+    print("----------- Character Count ----------")
+    totalCharCount = stats.get_book_charCounts(bookFileLocation)
+    for char in totalCharCount:
+      print(f"{char['char']}: {char['num']}")
+  except IndexError:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+  except Exception as e:
+    print(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
